c0ba1f6b00561b1c93460c6a27a53819347c0b79
liuhelei23 <liuhelei23@mails.tsinghua.edu.cn>
2025-11-11 20:52:32 +0800

    feat: 创建record模块及相关内容

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { BoardModule } from './modules/board/board.module';
+import { RecordModule } from './modules/record/record.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, BoardModule],
+  imports: [PrismaModule, UserModule, BoardModule, RecordModule],
   controllers: [AppController],
   providers: [AppService],
 })
